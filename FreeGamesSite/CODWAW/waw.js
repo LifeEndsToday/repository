b1779145b86ca6645aa2b83a0af6494211fdfbb2
LifeEndsToday <56409922+LifeEndsToday@users.noreply.github.com>
2019-10-10 21:52:13 +0300

    Add files via upload

---
+++ FreeGamesSite/CODWAW/waw.js
@@ -0,0 +1,5 @@
+/*let body = document.getElementById("body");
+let windowW = window.innerWidth
+let hi = ((windowW - 114) / 2).toString() + "px";
+body.style.borderLeft = "solid red";
+body.style.borderLeftWidth = "calc(hi)"*/
